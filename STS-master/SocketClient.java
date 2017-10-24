d0bd250d96b2dc45d14000d9bf15c18b40d89226
adambatt111 <adambatt111@outlook.com>
2017-10-24 23:14:23 +0100

    Created connection to host

---
+++ STS-master/SocketClient.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.net.Socket;
+
+public class SocketClient
+{
+	public static void main(String args[])
+	{
+		try
+		{
+			Socket clientSocket = new Socket("10.5.38.127", 5000);
+			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+			BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+			System.out.println("HELO");
+		}
+		        catch(IOException e)
+        {
+            System.out.println("Error in setting up socket " + e);
+            System.exit(1);
+        }
+	}
+}
+
