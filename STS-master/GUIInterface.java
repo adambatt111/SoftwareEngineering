15be8952f39ed2b9940fb4d729ed5b2727a3e409
Arthur Mitchell <arthurmitchell96@gmail.com>
2017-11-20 13:50:46 +0000

    Creation of basic user interface

---
+++ STS-master/GUIInterface.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.net.*;
+public class GUIInterface {
+
+	public static void main(String args[]){
+		
+		System.out.println("Welcome to the Stock take system");
+		System.out.println("Connecting to the system now");
+		Socket clientSocket;
+		try{
+		clientSocket = new Socket("10.5.41.31", 5000);
+		
+		
+		PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+		InputStreamReader inputGot = new InputStreamReader(clientSocket.getInputStream());			
+		BufferedReader in = new BufferedReader(inputGot);
+		
+		out.println("REGI");
+		String userInput;	
+		userInput = in.readLine();	
+			
+		System.out.println("UserID " + userInput);
+		
+		}
+		catch(Exception ex){
+			System.out.println("Could not connect to the server. Make sure server is online");
+			System.out.println("Error:" + ex);
+			System.out.println("Closing program now...");
+			System.exit(0);
+		}
+		
+		
+		//insert get id here 
+		//display id here
+		
+		boolean programOn = true;
+		while (programOn == true){
+			System.out.println("What would you like to do? \n 1. Buy Stocks(will be implemented soon) \n 2. Sell Stocks(will be implemented soon)\n 3. Look at stocks (will be implemented soon)\n 4. Live feed (will be implemented soon) \n 5. list profits (will be implemented soon) \n 6. Veiw money (will be implemented soon)\n7. View owned shares (will be implemented soon) \n8. past 5 changes (will be implemented soon)\n9. exit\n");
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			try{
+				String input = br.readLine();
+				switch(input){
+					case "1":
+					System.out.println("Buying Stocks");
+					programOn = false;
+					break;
+					case "2":
+					System.out.println("Selling Stocks");
+					programOn = false;
+					break;
+					case "3":
+					System.out.println("Looking at Stocks");
+					programOn = false;
+					break;
+					case "4":
+					System.out.println("Live Feed");
+					programOn = false;
+					break;
+					case "5":
+					System.out.println("My Profits");
+					programOn = false;
+					break;
+					case "6":
+					System.out.println("Money");
+					programOn = false;
+					break;
+					case "7":
+					System.out.println("Owned Shares");
+					programOn = false;
+					break;
+					case "8":
+					System.out.println("Past 5 Changes");
+					programOn = false;
+					break;
+					case "9":
+					System.exit(0);
+					//Call exit from Server before quitting
+					programOn = false;
+					break;
+					default:
+					System.out.println("\nPlease Choose Valid Option\n");
+					programOn = true;
+					break;					
+				}
+			}catch(Exception e){
+				
+			}
+		}
+		
+	}
+	
+}
